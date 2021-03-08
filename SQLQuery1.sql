adf152f9bedc5948670193132317c77fd110802b
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-03-08 20:43:52 +0530

    [vijay]AddedDataInTable

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE Payroll_Service;
+select * from sys.databases;
 USE Payroll_Service;
+
 create table employee_payroll(
 id int not null identity(1,1) primary key,
 name varchar(150) not null,
@@ -7,3 +9,16 @@ salary bigint not null,
 start date not null,
 );
 
+insert into employee_payroll (name, salary, start)values 
+('Ramesh', 15000, '2001-10-01');
+
+select * from employee_payroll;
+
+insert into employee_payroll (name, salary, start)
+values ('Sanju', 10000, '2003-08-21');
+
+insert into employee_payroll (name, salary, start)values 
+('Dilip', 17000, '2002-06-14');
+
+insert into employee_payroll (name, salary, start)values 
+('Sachin', 7000, '2001-12-08');

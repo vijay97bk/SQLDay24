4b92e6cb40ae56922fd559f5d78e3f36545aa674
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-03-08 14:01:25 +0530

    [vijay]CreatedNewTable

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,9 @@
 CREATE DATABASE Payroll_Service;
 USE Payroll_Service;
+create table employee_payroll(
+id int not null identity(1,1) primary key,
+name varchar(150) not null,
+salary bigint not null,
+start date not null,
+);
 
